877cf3e4482f03e9a53fe7a704bd8000de79cf91
Rujuta Deshpande <rujuta.deshpande@outlook.com>
2022-05-30 18:28:17 -0700

    tets

--- DemoFramework/src/main/java/controls/testdata.java
+++ DemoFramework/src/main/java/controls/testdata.java
@@ -4,7 +4,7 @@ public class testdata {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ System.out.println("New update");
 		System.out.print("Checking git hub...");
 	}
 
